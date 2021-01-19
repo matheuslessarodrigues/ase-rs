255568ec314bebac680a970267441f37a3edb912
Jakob Schwab <j.schwab@mail.de>
2021-01-19 16:08:24 +0100

    Using `fixed` crate for reading and writing Aseprite fixed point numbers.
    Adds testfile which uses `ColorProfileChunk` that contains a fixed point gamm value

--- src/chunk/color_profile_chunk.rs
+++ src/chunk/color_profile_chunk.rs
@@ -1,10 +1,14 @@
+pub use fixed::{types::extra::U16, FixedU32};
+
 use std::io::{self, Read, Seek, SeekFrom, Write};
 
 use bitflags::bitflags;
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use num_enum::CustomTryInto;
 
-use crate::helpers::read_bytes;
+use crate::helpers::{
+    read_bytes, read_fixed_point_number_16_16, write_fixed_point_number_16_16,
+};
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, CustomTryInto)]
 #[repr(u16)]
@@ -24,11 +28,15 @@ bitflags! {
 pub struct ColorProfileChunk {
     pub profile_type: ProfileType,
     pub flags: Flags,
-    pub fixed_gamma: f32,
+    pub fixed_gamma: FixedU32<U16>,
     pub icc_profile: Vec<u8>,
 }
 
 impl ColorProfileChunk {
+    pub fn fixed_gamma(&self) -> f32 {
+        self.fixed_gamma.to_num::<f32>()
+    }
+
     pub fn from_read<R>(read: &mut R) -> io::Result<Self>
     where
         R: Read + Seek,
@@ -38,7 +46,7 @@ impl ColorProfileChunk {
             .try_into_ProfileType()
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
         let flags = Flags::from_bits_truncate(read.read_u16::<LittleEndian>()?);
-        let fixed_gamma = read.read_f32::<LittleEndian>()?;
+        let fixed_gamma = read_fixed_point_number_16_16(read)?;
         read.seek(SeekFrom::Current(8))?;
 
         let icc_profile = if profile_type == ProfileType::EmbeddedIccProfile {
@@ -62,7 +70,7 @@ impl ColorProfileChunk {
     {
         wtr.write_u16::<LittleEndian>(self.profile_type as u16)?;
         wtr.write_u16::<LittleEndian>(self.flags.bits)?;
-        wtr.write_f32::<LittleEndian>(self.fixed_gamma)?;
+        write_fixed_point_number_16_16(wtr, &self.fixed_gamma)?;
         wtr.seek(SeekFrom::Current(8))?;
         wtr.write(&self.icc_profile)?;
         Ok(())
--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,7 @@
 use std::io::{self, Read, Write};
 
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+pub use fixed::{types::extra::U16, FixedU32};
 
 pub fn read_bytes<R>(read: &mut R, length: usize) -> io::Result<Vec<u8>>
 where
@@ -10,6 +11,28 @@ where
     bytes.resize(length, 0);
     read.read_exact(&mut bytes[..])?;
     Ok(bytes)
+}
+
+pub fn read_fixed_point_number_16_16<R>(
+    read: &mut R,
+) -> io::Result<FixedU32<U16>>
+where
+    R: Read,
+{
+    let bits = read.read_u32::<LittleEndian>()?;
+    Ok(FixedU32::<U16>::from_bits(bits))
+}
+
+pub fn write_fixed_point_number_16_16<W>(
+    wtr: &mut W,
+    number: &FixedU32<U16>,
+) -> io::Result<()>
+where
+    W: Write,
+{
+    let bits = number.to_bits();
+    wtr.write_u32::<LittleEndian>(bits)?;
+    Ok(())
 }
 
 pub fn read_string<R>(read: &mut R) -> io::Result<String>
